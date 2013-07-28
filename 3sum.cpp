20f3291cf71fe413b046da647afcbd6128ec1e70
vincentpc <peter5547825@gmail.com>
2013-07-28 16:30:22 +0800

    Create 3sum.cpp

---
+++ 3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int> &num, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int n = num.size();
+        int close = -1000000,closetarget;
+        sort(num.begin(),num.end());
+        int i,j,k;
+        for(i = 0;i < n; i ++){
+            
+            j = i + 1;
+            k = n - 1;
+            
+            while(j < k){3=
+                closetarget = num[i] + num[j] + num[k];
+                if(abs(closetarget-target) < abs(close-target)){
+                    close = closetarget;
+                }
+                
+                if(closetarget < target)
+                    j++;
+                else
+                    k--;
+            
+            }
+        }
+        return close;
+        
+    }
+};
