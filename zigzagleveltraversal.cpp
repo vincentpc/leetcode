57b893b4b52dab71600bb269b45f67836c0c14e5
vincentpc <peter5547825@gmail.com>
2013-07-30 20:46:38 +0800

    Create zigzagleveltraversal.cpp

---
+++ zigzagleveltraversal.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    struct ele{
+        TreeNode *node;
+        int depth;
+    };
+    
+    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        vector<vector<int> >results;
+        vector<int> result;
+        queue<ele> q;
+        int depthnow;
+        
+        if(root == NULL)
+            return results;
+        
+        ele rootele;
+        rootele.node = root;
+        rootele.depth = 0;
+        depthnow = 0;
+        q.push(rootele);
+        int binarybit = 0;
+        
+        while(!q.empty()){
+            ele nownode = q.front();
+            q.pop();
+            
+            if(nownode.depth == depthnow){
+                result.push_back(nownode.node->val);
+            }
+            else{
+                if(binarybit == 1){
+                    reverse(result.begin(),result.end());
+                }
+                binarybit++;
+                binarybit %= 2;
+                
+                results.push_back(result);
+                result.clear();
+                result.push_back(nownode.node->val);
+                depthnow = nownode.depth;
+            }
+            
+            if(nownode.node->left != NULL){
+                ele leftele;
+                leftele.node = nownode.node->left;
+                leftele.depth = nownode.depth+1;
+                q.push(leftele);
+            }
+            if(nownode.node->right != NULL){
+                ele rightele;
+                rightele.node = nownode.node->right;
+                rightele.depth = nownode.depth+1;
+                q.push(rightele);
+            }
+        }
+        
+        if(result.size() > 0){
+            if(binarybit == 1){
+                reverse(result.begin(),result.end());
+            }
+            results.push_back(result);
+        }
+        return results;
+    
+    }
+};
