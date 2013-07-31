95e1cc95edadd131322601400d96f0f89f12f681
vincentpc <peter5547825@gmail.com>
2013-07-31 18:48:39 +0800

    Create combinationsum.cpp

---
+++ combinationsum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int> > results;
+    vector<int> result;
+    int targetsum,size;
+    vector<int> can;
+    
+    void test(vector<int> result,int beg,int k){
+        if(k > targetsum)
+            return;
+            
+        if(k == targetsum){
+            if(find(results.begin(),results.end(),result) == results.end())
+                results.push_back(result);
+            return;
+        }
+        
+        
+        for(int i = beg; i < size; i ++){
+            result.push_back(can[i]);
+            test(result,i,k + can[i]);
+            result.pop_back();
+        }
+        
+        return;
+    }
+    
+    
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        results.clear();
+        sort(candidates.begin(), candidates.end());
+        targetsum = target;
+        size = candidates.size();
+        can = candidates;
+        
+        result.clear();
+        test(result,0,0);
+        return results;
+        
+        
+    }
+};
