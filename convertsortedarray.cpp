1d5e8d91308e89f2cf57e1e6bd206840e5457e6a
vincentpc <peter5547825@gmail.com>
2013-07-31 22:14:41 +0800

    Create convertsortedarray.cpp

---
+++ convertsortedarray.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> array;
+    
+    TreeNode * test(int begin, int end){
+        
+        TreeNode * root = new TreeNode(0);
+        int mid = (begin+end)/2;
+        root->val = array[mid];
+        if(mid - 1 >= begin){
+            root->left = test(begin,mid-1);
+        }
+        if(mid + 1 <= end){
+            root->right = test(mid+1,end);
+        }
+        
+        return root;
+        
+    }
+    
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        array = num;
+        TreeNode * root = NULL;
+        if(num.size() == 0)
+            return root;
+            
+        root = test(0,num.size()-1);
+        
+        return root;
+    }
+};
