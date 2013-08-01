e92814abb45ff0f0dc5b5d5ebae1065e020d233c
vincentpc <peter5547825@gmail.com>
2013-08-01 16:46:43 +0800

    Update decodeway.cpp

--- decodeway.cpp
+++ decodeway.cpp
@@ -1,29 +1,36 @@
+
 class Solution {
 public:
     int num;
-    
-    void test(string s, int beg){
+    int mark[100000];
+    int test(string s, int beg){
         if(beg == s.length()){
-            num++;
-            return;
+            //num++;
+            return 1;
         }
+        
+        int first = 0,second = 0;
+        
+        if(mark[beg] != 0)
+            return mark[beg];
+        
         
         if(beg < s.length()){
             int now = s[beg]-'0';
             if(now >= 1 && now <= 9)
-                test(s,beg+1);
+                first = test(s,beg+1);
         }
         
         if(beg < s.length() -1){
-            int first = s[beg]-'0';
-            
-            if(first == 0)
-                return;
             int now = (s[beg]-'0') * 10 + (s[beg+1]-'0');
-            if(now <= 26 && now > 0)
-                test(s,beg+2);
+            if(now <= 26 && now >= 10)
+                second = test(s,beg+2);
         }
-        return;
+        
+        mark[beg] = first+second;
+        
+        return first+second;
+        
         
     }
     
@@ -31,13 +38,14 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         num = 0;
+        memset(mark,0,sizeof(mark));
         
         if(s.length() == 0)
             return num;
             
         test(s,0);
         
-        return num;
+        return mark[0];
         
     }
 };
