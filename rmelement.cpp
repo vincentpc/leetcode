88082b3279f0c85a804b1e3b3e8e3ead94a6ad5f
vincentpc <peter5547825@gmail.com>
2013-08-11 15:19:24 +0800

    Create rmelement.cpp

---
+++ rmelement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int left = 0;
+        for(int i = 0; i < n; i ++){
+            if(A[i] == elem){
+                
+            }
+            else{
+                A[left++] = A[i];
+            }
+        }
+        return left;
+    }
+};
