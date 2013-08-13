96e62773bb7c47cae5cc384b9440dc9be6533def
vincentpc <peter5547825@gmail.com>
2013-08-13 22:52:15 +0800

    Update scramblestr.cpp

--- scramblestr.cpp
+++ scramblestr.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
+        bool samecheck(string left,string right){
+        if(left.length() != right.length())
+            return false;
+            
+        sort(left.begin(),left.end());
+        sort(right.begin(),right.end());
+        
+        if(left == right)
+            return true;
+        else return false;
+        
+    }
     bool isScramble(string s1, string s2) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        
         if(s1.size() != s2.size() )
             return false;
+            
+        if(s1.size() == 0)
+            return true;
             
         if(s1.size() == 1){
             if(s1[0] == s2[0])
@@ -19,8 +35,12 @@ public:
             return false;
         }
         
+        if(samecheck(s1,s2) == false)
+            return false;
+        
         int mid;
         for(mid = 1; mid <= s1.size()-1; mid ++){
+ 
             string sub11 = s1.substr(0,mid);
             string sub12 = s1.substr(mid,s1.size() - mid);
             string sub21 = s2.substr(0,mid);
