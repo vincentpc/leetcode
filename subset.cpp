56fcc9e89c43b219317046ca9761510552ba349a
vincentpc <peter5547825@gmail.com>
2013-08-17 16:25:16 +0800

    Create subset.cpp

---
+++ subset.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int> > results;
+    int n;
+    vector<int> s;
+    void test(vector<int> res, int begin){
+        if(begin == n){
+            results.push_back(res);
+            return;
+        }
+        
+        test(res,begin+1);
+        res.push_back(s[begin]);
+        test(res,begin+1);
+        return;
+        
+    }
+    
+    vector<vector<int> > subsets(vector<int> &S) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        results.clear();
+        
+        if(S.size() == 0){
+            return results;
+        }
+        
+        n = S.size();
+        s = S;
+        
+        vector<int> result;
+        sort(s.begin(),s.end());
+        
+        test(result,0);
+        
+        return results;
+        
+    }
+};
